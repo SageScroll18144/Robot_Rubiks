ae92be5144afaa394cb723baccc54c6228071de6
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-12-31 09:47:38 -0300

    Implementado Fd1. {Dd1;Ds2;Dd3}

--- IA/src/cube/IA/Fridrich.java
+++ IA/src/cube/IA/Fridrich.java
@@ -33,27 +33,26 @@ public class Fridrich {
 	
 	public void F2L() {
 		if(Integer.parseInt(String.valueOf(m.getPos(4, 3).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 8).charAt(m.getPos(4, 3).length()-1))) == 3 &&
+				Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 4).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 6).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 7).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 8).charAt(m.getPos(4, 8).length()-1))) == 3 &&
 				Character.getNumericValue(m.indexof(5, 3).charAt(5)) == 4 && 
 				Character.getNumericValue(m.indexof(5, 3).charAt(m.indexof(5, 3).length()-1)) == 5 &&
 				
-				Integer.parseInt(String.valueOf(m.getPos(3, 4).charAt(m.getPos(3, 3).length()-1))) == 5 && 
-				Integer.parseInt(String.valueOf(m.getPos(3, 5).charAt(m.getPos(3, 3).length()-1))) == 5 && 
-				Integer.parseInt(String.valueOf(m.getPos(3, 6).charAt(m.getPos(3, 3).length()-1))) == 5 && 
-				Integer.parseInt(String.valueOf(m.getPos(3, 7).charAt(m.getPos(3, 3).length()-1))) == 5 && 
-				Integer.parseInt(String.valueOf(m.getPos(3, 8).charAt(m.getPos(3, 3).length()-1))) == 5 
+				Integer.parseInt(String.valueOf(m.getPos(3, 4).charAt(m.getPos(3, 4).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 5).charAt(m.getPos(3, 5).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 6).charAt(m.getPos(3, 6).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 7).charAt(m.getPos(3, 7).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 8).charAt(m.getPos(3, 8).length()-1))) == 5 
 				) {
 				//R U' R' Dw R' U2 R U2' R' U R
-		}
-		
+		}		
 		else if(Integer.parseInt(String.valueOf(m.getPos(4, 3).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 3).length()-1))) == 3 && 
-				Integer.parseInt(String.valueOf(m.getPos(4, 8).charAt(m.getPos(4, 3).length()-1))) == 3 &&
+				Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 4).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 6).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 7).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 8).charAt(m.getPos(4, 8).length()-1))) == 3 &&
 				Character.getNumericValue(m.indexof(3, 5).charAt(5)) == 4 && 
 				Character.getNumericValue(m.indexof(3, 5).charAt(m.indexof(3, 5).length()-1)) == 1 &&
 				
@@ -81,6 +80,23 @@ public class Fridrich {
 				) {		
 				//U' L' U L Dw R U' R'
 			
+		}
+		else if(Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 4).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 3).charAt(m.getPos(4, 3).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 7).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 6).length()-1))) == 3 &&
+				Character.getNumericValue(m.indexof(3, 5).charAt(5)) == 4 && 
+				Character.getNumericValue(m.indexof(3, 5).charAt(m.indexof(5, 3).length()-1)) == 5 &&
+				
+				Integer.parseInt(String.valueOf(m.getPos(3, 6).charAt(m.getPos(3, 6).length()-1))) == 4 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 8).charAt(m.getPos(4, 8).length()-1))) == 3 && 
+		
+				Integer.parseInt(String.valueOf(m.getPos(3, 4).charAt(m.getPos(3, 4).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 5).charAt(m.getPos(3, 5).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 7).charAt(m.getPos(3, 7).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 8).charAt(m.getPos(3, 8).length()-1))) == 5
+				) {
+				//R2 U2 R' U' R U' R' U2 R'
 		}
 		
 		
