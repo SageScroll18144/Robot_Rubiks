905e05d4daf724fad123df5324ab20b94ab48267
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-12-31 11:56:22 -0300

    Rd2 implementado

--- IA/src/cube/IA/Fridrich.java
+++ IA/src/cube/IA/Fridrich.java
@@ -172,9 +172,25 @@ public class Fridrich {
 				Character.getNumericValue(m.indexof(3, 5).charAt(5)) == 4 && 
 				Character.getNumericValue(m.indexof(3, 5).charAt(m.indexof(5, 3).length()-1)) == 5	
 				) {
-				//
+				//R U' R' U R U' R'
+		}//Rd2
+		else if(Integer.parseInt(String.valueOf(m.getPos(4, 3).charAt(m.getPos(4, 3).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 4).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 6).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 7).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 6).charAt(m.getPos(3, 6).length()-1))) == 4 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 8).charAt(m.getPos(4, 8).length()-1))) == 5 &&
+				
+				Integer.parseInt(String.valueOf(m.getPos(3, 4).charAt(m.getPos(3, 4).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 5).charAt(m.getPos(3, 5).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 7).charAt(m.getPos(3, 7).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 8).charAt(m.getPos(3, 8).length()-1))) == 5 &&
+				
+				Character.getNumericValue(m.indexof(5, 3).charAt(5)) == 4 && 
+				Character.getNumericValue(m.indexof(5, 3).charAt(m.indexof(5, 3).length()-1)) == 5
+				) {
+				//R U' R' F' L' U2 L F
 		}
-		
 		
 		
 	}
