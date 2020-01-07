7c57dddfb55e4067f80eeea4841c8b958e923786
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-07 11:48:58 -0300

    Implementado F5

--- IA/src/cube/IA/Fridrich.java
+++ IA/src/cube/IA/Fridrich.java
@@ -661,7 +661,26 @@ public class Fridrich {
 				Character.getNumericValue(m.indexof(4, 3, 5).charAt(m.indexof(4, 3, 5).length()-1)) == 2
 				) {
 			//U' R U2 R' Dw R' U' R
-		}
+		}//F5 centro trocado
+		else if(Integer.parseInt(String.valueOf(m.getPos(4, 3).charAt(m.getPos(4, 3).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 4).charAt(m.getPos(4, 4).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 6).charAt(m.getPos(4, 6).length()-1))) == 3 && 
+				Integer.parseInt(String.valueOf(m.getPos(4, 7).charAt(m.getPos(4, 7).length()-1))) == 3 &&
+				
+				Integer.parseInt(String.valueOf(m.getPos(3, 4).charAt(m.getPos(3, 4).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 5).charAt(m.getPos(3, 5).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 7).charAt(m.getPos(3, 7).length()-1))) == 5 && 
+				Integer.parseInt(String.valueOf(m.getPos(3, 8).charAt(m.getPos(3, 8).length()-1))) == 5 &&
+				
+				Character.getNumericValue(m.indexof(5, 3).charAt(5)) == 4 &&
+				Character.getNumericValue(m.indexof(5, 3).charAt(m.indexof(5, 3).length()-1)) == 1 &&
+		
+				Character.getNumericValue(m.indexof(4, 3, 5).charAt(5)) == 4 &&
+				Character.getNumericValue(m.indexof(4, 3, 5).charAt(m.indexof(4, 3, 5).length()-1)) == 2
+				) {
+			//L' U L U' Dw' L U L'
+		}//F6 centro trocado
+		
 	}
 
 	
