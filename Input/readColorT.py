96e6c415269861cb761f1e7f0d4ba1a087a948bd
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-07 15:48:06 -0300

    Segmentando

--- Input/readColorT.py
+++ Input/readColorT.py
@@ -10,10 +10,13 @@ while(True):
     ret, frame = cam.read()
  	
     #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #pré-processamento
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gauss = cv2.GaussianBlur(gray, (3, 3), 0)
     segmentationCanny = cv2.Canny(gauss, 20, 40)
     
+    #dilatando
     kernel = np.ones((3, 3), np.uint8)
     dilate = cv2.dilate(segmentationCanny, kernel, iterations=2)
 
