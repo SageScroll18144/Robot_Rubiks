0505fdcc817170cca4c2a66f4e013d62290b7155
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-12 10:17:22 -0300

    OLL 12

--- IA/src/cube/IA/Fridrich.java
+++ IA/src/cube/IA/Fridrich.java
@@ -535,6 +535,18 @@ public class Fridrich {
 				Integer.parseInt(String.valueOf(m.getPos(3, 3).charAt(m.getPos(3, 3).length()-1))) == 0 
 				) {
 			//R' U' F' U F R
+		}//12
+		else if(Integer.parseInt(String.valueOf(m.getPos(4, 2).charAt(m.getPos(4, 2).length()-1))) == 0 &&
+				Integer.parseInt(String.valueOf(m.getPos(4, 1).charAt(m.getPos(4, 1).length()-1))) == 0 &&
+				Integer.parseInt(String.valueOf(m.getPos(4, 5).charAt(m.getPos(4, 5).length()-1))) == 0 &&
+				
+				Integer.parseInt(String.valueOf(m.getPos(1, 2).charAt(m.getPos(1, 2).length()-1))) == 0 &&
+				Integer.parseInt(String.valueOf(m.getPos(1, 5).charAt(m.getPos(1, 5).length()-1))) == 0 &&
+				Integer.parseInt(String.valueOf(m.getPos(5, 0).charAt(m.getPos(5, 0).length()-1))) == 0 &&
+				Integer.parseInt(String.valueOf(m.getPos(5, 1).charAt(m.getPos(5, 1).length()-1))) == 0 &&
+				Integer.parseInt(String.valueOf(m.getPos(3, 6).charAt(m.getPos(3, 3).length()-1))) == 0 
+				) {
+			//Rw U2 R' U' R U' Rw'
 		}
 		else {
 			m.moveAxisYNegative();
