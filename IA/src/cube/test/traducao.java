1160685d62c4f85d3f66ac08bdfcee0b953ac27e
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-21 19:24:35 -0300

    Erro incompreendido

--- IA/src/cube/test/traducao.java
+++ IA/src/cube/test/traducao.java
@@ -7,8 +7,8 @@ public class traducao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Translate t = new Translate();
-		
-		System.out.println(t.getTranslationToComputer("D"));
+		String a = "B";
+		System.out.println(t.getTranslationToComputer(a));
 		System.out.println(t.getMoveToComputer());
 		System.out.println(t.getTranslationToCube("2R"));
 		System.out.println(t.getMoveToCube());
--- IA/src/cube/user/Emulador.java
+++ IA/src/cube/user/Emulador.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 import cube.move.Movimento;
 import cube.language.TranslateCompound;
+import cube.language.Translate;
 
 public class Emulador {
 
 	public static void main(String[] args) {
 		
 		int[][] cube = {{0,0,0,0,0,0,0,0,0},{1,1,1,1,1,1,1,1,1},{2,2,2,2,2,2,2,2,2},{3,3,3,3,3,3,3,3,3},{4,4,4,4,4,4,4,4,4},{5,5,5,5,5,5,5,5,5}};  
-	        
+
 		Movimento m = new Movimento(cube);
 		TranslateCompound t = new TranslateCompound(m);
+		Translate ts = new Translate();
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.print("***EMULADOR DO CUBO MÁGICO!***\nInforme 1-Linguagem do Computador ou 2-Notação de Cubo Mágico.: ");
 		String answer = s.nextLine();
 		
@@ -29,7 +31,7 @@ public class Emulador {
 					if(input.charAt(1) == 'R') {
 						m.spinRightFront(Character.getNumericValue(input.charAt(0)));
 					}else if(input.charAt(1) == 'L') {
-						m.spinRightFront(Character.getNumericValue(input.charAt(0)));
+						m.spinLeftBack(Character.getNumericValue(input.charAt(0)));
 					}
 				}
 				else {
@@ -40,14 +42,24 @@ public class Emulador {
 			}
 		}else if(answer.equals("2")) {
 			while(!"end".equals(input = s.nextLine())) {
-				
 				System.out.println(input);
+				String a = String.valueOf(input);
+				System.out.println(a);
+				System.out.println(ts.getTranslationToComputer(a));
+				if(input.charAt(0) == 'x' || input.charAt(0) == 'y' || input.charAt(0) == 'z' || (input.length() > 1 && input.charAt(1) == 'w')) {
+					t.cases(input);
+				}else if(ts.getTranslationToComputer(input).charAt(1) == 'R'){
+					m.spinRightFront(Character.getNumericValue(ts.getTranslationToComputer(input).charAt(0)));
+				}else if(ts.getTranslationToComputer(input).charAt(1) == 'L') {
+					m.spinLeftBack(Character.getNumericValue(ts.getTranslationToComputer(input).charAt(0)));
+				}
+				m.print();
 				System.out.print("Informe o movimento ou 'end' como valor para sair do programa.: ");
 			}
 		}else {
 			System.err.println("Valor fora do bloco lógico");
 		}
-		
+		s.close();
 		System.out.println(m.getMove());
 	}
 	
