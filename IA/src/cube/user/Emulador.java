e011d94912107cabbd372688753f53fc527d6cca
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-21 17:55:31 -0300

    LOOP

--- IA/src/cube/user/Emulador.java
+++ IA/src/cube/user/Emulador.java
@@ -1,10 +1,24 @@
 package cube.user;
 
+import java.util.Scanner;
+
 public class Emulador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("***EMULADOR DO CUBO MÁGICO!***\nInforme 1-Linguagem do Computador ou 2-Notação de Cubo Mágico.: ");
+		String answer = s.nextLine();
+		
+		System.out.print("Informe o movimento.: ");
+		String input;
+		
+		while(!"end".equals(input = s.nextLine())) {
+
+			System.out.println(input);
+			
+		}
 
 	}
-
+	
 }
