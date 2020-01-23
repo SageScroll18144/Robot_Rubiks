42a7e894760a1b012f5389aeb99ade8c214c4904
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-01-23 14:17:28 -0300

    movimentos

--- IA/src/cube/IA/Fridrich.java
+++ IA/src/cube/IA/Fridrich.java
@@ -1,10 +1,15 @@
 package cube.IA;
 
+import cube.language.Translate;
+import cube.language.TranslateCompound;
 import cube.move.Movimento;
 
 public class Fridrich {
 	
 	private Movimento m;
+	private TranslateCompound t = new TranslateCompound(m);
+	private Translate ts = new Translate();
+
 	public Fridrich(Movimento m) {
 		this.m = m;
 	}
@@ -1120,4 +1125,15 @@ public class Fridrich {
 	public void caseOLL() {
 		OLL();
 	} 
+	private void performsMovements(String inputs) {
+		for (String input : inputs.split(" ")) {
+			if(input.charAt(0) == 'x' || input.charAt(0) == 'y' || input.charAt(0) == 'z' || input.charAt(0) == 'M' || input.charAt(0) == 'S' ||input.charAt(0) == 'E' || (input.length() > 1 && input.charAt(1) == 'w')) {
+				t.cases(input);
+			}else if(ts.getTranslationToComputer(input).charAt(1) == 'R'){
+				m.spinRightFront(Character.getNumericValue(ts.getTranslationToComputer(input).charAt(0)));
+			}else if(ts.getTranslationToComputer(input).charAt(1) == 'L') {
+				m.spinLeftBack(Character.getNumericValue(ts.getTranslationToComputer(input).charAt(0)));
+			}
+		}
+	}
 }
